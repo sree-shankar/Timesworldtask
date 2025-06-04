4c1e6c187baf562418ef956f0cf9d8bfc1bbc980
sree-shankar <mmsreeshankar@gmail.com>
2025-06-04 16:27:28 +0530

    hashrouter fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 
